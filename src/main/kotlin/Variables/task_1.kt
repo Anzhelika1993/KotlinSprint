6b21b729594cfd5b390f6f0975088dea7cd52a9b
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-10 22:28:51 +0700

    KS-1-1 заменила var на val и убрала пустой блок

--- src/main/kotlin/Variables/task_1.kt
+++ src/main/kotlin/Variables/task_1.kt
@@ -1,7 +1,6 @@
 package org.example.Variables
 
 fun main () {
-    var numberOfOrders: Int = 75
+    val numberOfOrders: Int = 75
     val thanksForOrder: String = "Спасибо, что выбрали наш магазин!"
-
        }
