cd21ed2ef460b85db8906301705c8cb5061d2c58
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-10 23:19:00 +0700

    KS-1-3 изменила значения переменных hour и minute, и вывела время посадки

--- src/main/kotlin/Variables/task_3.kt
+++ src/main/kotlin/Variables/task_3.kt
@@ -1,12 +1,11 @@
 package org.example.Variables
 
-fun main () {
-    val yearOfSpaceflight = 1961
-    var start = "09:07"
-    var landing = "10:55"
-
-    println("Год полета - $yearOfSpaceflight")
-    println("Взлет - $start")
-    println("Посадка - $landing")
-
+fun main() {
+    val yearOfSpaceflight: String = "1961"
+    var hour = "10"
+    var minute = "55"
+    println(yearOfSpaceflight)
+    println(hour)
+    println(minute)
+    println("$hour:$minute")
 }
