da10be816c6fdc819e4dea9e71ed850e7d32d08d
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-10 22:36:42 +0700

    KS-1-2 убрала лишний пробел перед -- и удалила пустые строки

--- src/main/kotlin/Variables/task_2.kt
+++ src/main/kotlin/Variables/task_2.kt
@@ -1,18 +1,12 @@
 package org.example.Variables
 
-fun main () {
-    var numberOfOrders: Int = 75
+fun main() {
+    val numberOfOrders: Int = 75
     val thanksForOrder: String = "Спасибо, что выбрали наш магазин!"
     println(numberOfOrders)
     println(thanksForOrder)
-
     var numberOfEmployees = 2000
     //println(numberOfEmployees) один работник уволился
-
-    numberOfEmployees --
+    numberOfEmployees--
     println(numberOfEmployees)
-
-
-
-
-       }
+}
