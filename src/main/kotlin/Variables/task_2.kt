642f13be5fd7ecf03c46e8170ec50e244e031d75
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-10 21:35:15 +0700

    variables_task_2 created

---
+++ src/main/kotlin/Variables/task_2.kt
@@ -0,0 +1,19 @@
+package org.example.Variables
+
+
+fun main () {
+    var numberOfOrders: Int = 75
+    val thanksForOrder: String = "Спасибо, что выбрали наш магазин!"
+    println(numberOfOrders)
+    println(thanksForOrder)
+
+    var numberOfEmployees = 2000
+    //println(numberOfEmployees) один работник уволился
+
+    numberOfEmployees --
+    println(numberOfEmployees)
+
+
+
+
+       }
