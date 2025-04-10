bd4f125a9b39d0afdc9235ee598347377a65d143
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-10 21:59:12 +0700

    variables_task_3 created

---
+++ src/main/kotlin/Variables/task_3.kt
@@ -0,0 +1,12 @@
+package org.example.Variables
+
+fun main () {
+    val yearOfSpaceflight = 1961
+    var start = "09:07"
+    var landing = "10:55"
+
+    println("Год полета - $yearOfSpaceflight")
+    println("Взлет - $start")
+    println("Посадка - $landing")
+
+}
