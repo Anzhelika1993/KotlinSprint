ada394c0c1f34087d4b3ddb50c7b9545d6b8c14c
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-14 19:29:01 +0700

    KS-2-3 вынесла 60 в константу

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,25 +1,16 @@
 package org.example.lesson_2
 
-import kotlin.time.Duration.Companion.hours
-import kotlin.time.Duration.Companion.minutes
-
 fun main() {
-
-//    val depatureTimeHour = 9.hours
-//    val depatureTimeMinute = 39.minutes
-//    val timeInWay = 457.minutes
-//    val end = (9.hours + 39.minutes + 457.minutes)
-//
-//    println(end)
 
     val depatureHour = 9
     val depatureMinute = 39
-    val InWay = 457
-    val wayInHours = InWay / 60
-    val wayInMinute = InWay % 60
+    val inWay = 457
+    val minInHour = 60
+    val wayInHours = inWay / minInHour
+    val wayInMinute = inWay % minInHour
     val arrivalTime = depatureHour + wayInHours
-    val arrivalTime2 = (depatureMinute + wayInMinute) / 60
-    val arrivalTime3 = (depatureMinute + wayInMinute) % 60
+    val arrivalTime2 = (depatureMinute + wayInMinute) / minInHour
+    val arrivalTime3 = (depatureMinute + wayInMinute) % minInHour
     val arrivalTimeHour = arrivalTime + arrivalTime2
 
     println("$arrivalTimeHour:$arrivalTime3")
