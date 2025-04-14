62215a6c9fa4cf549b80666f5043d75372d2d7b5
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-14 20:49:53 +0700

    KS-3-1 решение задачи

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main (){
+
+    var greetingMorning = "Good Morning"
+    val userName = "Mr.Evans"
+
+    println("$greetingMorning, $userName!")
+
+    var greetingEvening = "Good Evening"
+
+    println("$greetingEvening, $userName!")
+}
