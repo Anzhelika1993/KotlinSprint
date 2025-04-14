fedd7152421343f7d3f727c0025cc653c514be0a
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-14 22:41:22 +0700

    KS-3-5 решение задачи

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,11 +3,11 @@ package org.example.lesson_3
 fun main() {
 
     var playerMove = "D2-D4;0"
-    var Field = "D2"
+    var field = "D2"
     var newField = "D4"
     var move = 0
 
-    println("Откуда: $Field")
+    println("Откуда: $field")
     println("Куда: $newField")
     println("Номер хода: $move")
 }
