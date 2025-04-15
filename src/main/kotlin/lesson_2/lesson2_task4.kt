8ae8a089c171334a013d374e0494fc43cbed5dcc
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-15 19:06:01 +0700

    KS-2-4 вынесла 100 в константу

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_2
 
+    const val HUNDRED_PERCENT = 100
+
 fun main() {
 
     var kristallOre = 7
     var ironOre = 11
     var buff = 20
-    var buffInPercent = 20/100.toDouble()
+    var buffInPercent = buff / HUNDRED_PERCENT.toDouble()
     var buffKristallOre = (kristallOre * buffInPercent).toInt()
     var buffIronOre = (ironOre * buffInPercent).toInt()
 
