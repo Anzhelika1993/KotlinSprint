50896c813fb70f84b33b0001d7481bc56a32a851
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-16 21:41:32 +0700

    KS-4-3 вынесла благоприятные ли условия в константы

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,14 +1,20 @@
 package org.example.lesson_4
 
+const val SUNNY_WEATHER = true
+const val OUT_DOOR_AWNINIG = true
+const val AIR_HUMIDITY_IN_PERCENT = 20
+const val SEASON = "Not winter"
+
 fun main() {
 
-    val sunnyWeather = true
-    val outdoorAwning = true
-    val airHumidityInPercent = 20
+    val TodayWeather = true
+    val awning = true
+    val airHumidity = 20
     val currentSeason = "Winter"
-
-    val enablingEnvironment = sunnyWeather && outdoorAwning &&
-            (airHumidityInPercent == 20) && (currentSeason != "Winter")
+    val enablingEnvironment = (SUNNY_WEATHER == TodayWeather) &&
+            (OUT_DOOR_AWNINIG == awning) &&
+            (AIR_HUMIDITY_IN_PERCENT == airHumidity) &&
+            (SEASON == currentSeason)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $enablingEnvironment")
 }
