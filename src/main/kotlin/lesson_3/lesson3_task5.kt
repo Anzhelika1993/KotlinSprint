9c5effd2f83a562d4cbcb9dd66c07563b7c0b1d4
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-16 21:00:33 +0700

    KS-3-5 распарсила строку на 3 переменные

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,11 +3,13 @@ package org.example.lesson_3
 fun main() {
 
     var playerMove = "D2-D4;0"
-    var field = "D2"
-    var newField = "D4"
-    var move = 0
+    var split = playerMove!!.split("-", ";")
 
-    println("Откуда: $field")
-    println("Куда: $newField")
-    println("Номер хода: $move")
+    var field = split.get(0)
+    var newField = split.get(1)
+    var move = split.get(2)
+
+    println(field)
+    println(newField)
+    println(move)
 }
