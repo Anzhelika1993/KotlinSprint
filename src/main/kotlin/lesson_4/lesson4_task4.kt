9be8a0612558baecd01f753119628ebfb276ecaf
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-17 20:17:42 +0700

    KS-4-4 тренировки по дням

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_4
+
+fun main() {
+
+    var day = 6
+    var parity = day % 2
+
+    println(
+        "Упражнения для рук: ${parity == 1} \n" +
+                "Упражнения для ног: ${parity == 0} \n" +
+                "Упражнения для спины: ${parity == 0} \n" +
+                "Упражнения для пресса: ${parity == 1}"
+    )
+}
