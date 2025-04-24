88c98a20bdb5670bbef38d1aac987c252d1e04b8
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-24 08:50:13 +0700

    KS-4-5 добавила константы для сравнения

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -2,22 +2,24 @@ package org.example.lesson_4
 
 const val CREW_MIN = 55
 const val CREW_MAX = 70
+const val SHIP_HAS_NO_DAMAGE = true
 const val PROVISIONS_MIN = 50
+const val GOOD_WEATHER = true
 
 fun main() {
-    val shipHasNoDamage = true
-    var crew = 60
+    var IsShipHasNoDamage = true
+    var crew = 65
     var cratesOfProvisions = 51
     val weatherConditions = true
 
-    val journey = (shipHasNoDamage &&
+    val journey = (IsShipHasNoDamage == SHIP_HAS_NO_DAMAGE) &&
             (crew in CREW_MIN..CREW_MAX) &&
             (cratesOfProvisions > PROVISIONS_MIN) &&
-            (weatherConditions || !weatherConditions)) ||
-            (!shipHasNoDamage &&
-                    (crew == CREW_MAX) &&
-                    weatherConditions &&
-                    (cratesOfProvisions >= PROVISIONS_MIN))
+            (weatherConditions == GOOD_WEATHER || weatherConditions != GOOD_WEATHER) ||
+            (IsShipHasNoDamage != SHIP_HAS_NO_DAMAGE) &&
+            (crew == CREW_MAX) &&
+            (weatherConditions == GOOD_WEATHER) &&
+            (cratesOfProvisions >= PROVISIONS_MIN)
 
     println("Может ли корабль отправиться в плавание? $journey")
 }
