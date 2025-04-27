f4553f6571b58ff40c28a5d2211c94c2aa4b1794
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-27 19:50:17 +0700

    KS-6-1 авторизация пользователя через do while

---
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_6
+
+fun main() {
+
+    println("Создайте логин и пароль")
+    val userLoginCreated = readln()
+    val userPasswordCreated = readln()
+
+    println("Введите ваш логин и пароль")
+    val userLogin = readln()
+    val userPassword = readln()
+
+    do {
+        println("Введите ваш логин и пароль")
+        val userLogin = readln()
+        val userPassword = readln()
+    } while (userLogin != userLoginCreated || userPassword != userPasswordCreated)
+    println("Авторизация прошла успешно")
+}
