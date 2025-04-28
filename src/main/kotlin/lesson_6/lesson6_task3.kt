367d3a810294b23645fa1c67ef60b7ded95d8837
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-28 23:17:08 +0700

    KS-6-3 переменная seconds уменьшается в println

--- src/main/kotlin/lesson_6/lesson6_task3.kt
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -6,7 +6,6 @@ fun main() {
 
     println("Введите количество секунд")
     var seconds = readln().toInt()
-    var secondsInMillis = seconds * MILLIS_IN_SECONDS
 
     while (seconds > 0) {
         println("Осталось секунд: ${seconds--}")
