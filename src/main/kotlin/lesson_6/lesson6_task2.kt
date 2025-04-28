8b98ea49b86adaa9f9f2d90d12ea39f900729c0d
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-28 20:14:26 +0700

    KS-6-2 убрала цикл. Только через sleep

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -4,11 +4,9 @@ const val MILLIS_IN_SEC = 1000
 fun main() {
 
     println("Введите количество секунд")
-    var seconds = readln().toInt()
-    var secondsInMillis = (seconds * MILLIS_IN_SEC).toLong()
+    val seconds = readln().toInt()
+    val secondsInMillis = (seconds * MILLIS_IN_SEC).toLong()
 
-    while (seconds > 0) {
-        Thread.sleep(secondsInMillis)
-        println("Прошло $seconds секунд"); break
-    }
+    Thread.sleep(secondsInMillis)
+    println("Прошло $seconds секунд")
 }
