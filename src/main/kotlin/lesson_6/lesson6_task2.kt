ecaf070a192a80c0510f7e7d75286d8bb429f781
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-29 20:56:49 +0700

    KS-6-2 убрала лишнюю переменную

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_6
 
 const val MILLIS_IN_SEC = 1000
+
 fun main() {
 
     println("Введите количество секунд")
-    val seconds = readln().toInt()
-    val secondsInMillis = (seconds * MILLIS_IN_SEC).toLong()
 
-    Thread.sleep(secondsInMillis)
+    val seconds = readln().toLong()
+    Thread.sleep(seconds * MILLIS_IN_SEC)
+
     println("Прошло $seconds секунд")
 }
