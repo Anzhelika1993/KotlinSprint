992f02a07ac11f95b8c49d5ca91f7f929a76d6ec
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-29 21:26:36 +0700

    KS-6-5 - return вместо break - отформатировала код

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -10,18 +10,17 @@ fun main() {
     do {
         val number1 = (N1..N2).random()
         val number2 = (N1..N2).random()
-        println("Докажите, что вы не бот. Решите пример $number1 + $number2")
-        val trueResult = (number1 + number2).toString()
-        val result = readln()
+
+        print("Докажите, что вы не бот. Решите пример $number1 + $number2 = ")
+
+        val result = readln().toInt()
+        val trueResult = (number1 + number2).toInt()
+
         if (result == trueResult) {
             println("Добро пожаловать");
-            break
-        } else {
-            --limit
-        }
-    } while (limit > 0)
-
-    if (limit == 0) {
+            return
+        } else --limit
         println("Доступ запрещен")
-    }
+
+    } while (limit > 0)
 }
