1e005077725c5017911c26be15faa1d3142ddef6
Anzhelika <anzhelika.kuihina@mail.ru>
2025-04-30 19:27:29 +0700

    KS-7-1 результат вывела в переменную

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -5,16 +5,18 @@ fun main() {
     val numbersRange = 0..9
     val lettersRange = 'a'..'z'
     var elements = 6
+    var pass = ""
 
     while (elements > 0) {
 
         if (elements % 2 != 0) {
             val random1 = numbersRange.random()
-            print(random1)
+            pass += random1
         } else {
             val random2 = lettersRange.random()
-            print(random2)
+            pass += random2
         }
         elements--
     }
+    println(pass)
 }
