00965cd6dd6497e1553a30b1c3900bd071bc249c
Anzhelika <anzhelika.kuihina@mail.ru>
2025-05-02 22:35:54 +0700

    KS-7-2 вынесла переменную за пределы цикла

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -3,16 +3,17 @@ package org.example.lesson_7
 fun main() {
 
     val range = 1000..9999
-    val rangeRandom = range.random()
+    var rangeRandom = range.random()
 
     do {
+        rangeRandom = range.random()
         println("Ваш код авторизации: $rangeRandom")
         print("Введите код авторизации: ")
 
         val id = readln().toInt()
 
         if (id != rangeRandom) {
-            println("Неверно")
+                   println("Неверно")
         } else {
             println("Добро пожаловать")
             return
