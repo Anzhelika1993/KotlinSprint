ad0f92401e0d302c3099a228db12c921ed38a21d
Anzhelika <anzhelika.kuihina@mail.ru>
2025-05-04 18:39:22 +0700

    KS-7-5

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -4,27 +4,25 @@ const val MIN_SYMBOLS = 6
 
 fun main() {
 
-    val numbers = 0..9
+    val numbers = '0'..'9'
     val letters = 'a'..'z'
     val capitalLetters = 'A'..'Z'
     val charSet = numbers + letters + capitalLetters
 
     print("Задайте длину пароля ")
 
-    val numberOfSymbols = readln().toInt()
-    val pass = mutableListOf<String>()
-    pass.add(numbers.random().toString())
-    pass.add(letters.random().toString())
-    pass.add(capitalLetters.random().toString())
+    var numberOfSymbols = readln().toInt()
 
-    if (numberOfSymbols < MIN_SYMBOLS) {
-        println("Минимальная длина пароля 6 символов")
-    } else {
+    if (numberOfSymbols < MIN_SYMBOLS) numberOfSymbols = MIN_SYMBOLS
+
+    val pass = mutableListOf<Char>()
+    pass.add(numbers.random())
+    pass.add(letters.random())
+    pass.add(capitalLetters.random())
+
         for (i in 4..numberOfSymbols) {
-            val random = charSet.random().toString()
-            pass.add(random)
+            pass.add(charSet.random())
         }
         pass.shuffle()
         println(pass.joinToString(separator = ""))
     }
-}
