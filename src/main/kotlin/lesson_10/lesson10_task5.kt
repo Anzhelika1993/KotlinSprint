b118e4ad1143ce50f2b4dde188fe67d5fa062594
Anzhelika <anzhelika.kuihina@mail.ru>
2025-05-15 23:10:06 +0700

    KS-10-5 авторизация, токен, корзина

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -3,20 +3,36 @@ package org.example.lesson_10
 const val USER_LOGIN = "Angelica1993"
 const val USER_PASSWORD = "trulala2025"
 
-fun authorization(login: String, password: String): String? {
+fun authorization(log: String, pass: String): String? {
 
     val letters = 'a'..'z'
     val capLetters = 'A'..'Z'
     val numbers = '0'..'9'
     val setOfSymbols = letters + capLetters + numbers
-    val token = mutableListOf<Char>()
-    if (login == USER_LOGIN && password == USER_PASSWORD) {
+    var token: String? = null
+    if (log == USER_LOGIN && pass == USER_PASSWORD) {
+        val chars = mutableListOf<Char>()
         for (i in 0..32) {
-            token.add(setOfSymbols.random())
+            chars.add(setOfSymbols.random())
         }
-            token.joinToString(separator = "")
+        token = chars.joinToString(separator = "")
     }
     return token
+}
+
+fun basket(token: String?): String {
+
+    val shoppingList = listOf<String>(
+        "Sirius Adult Ягненок 2 кг", "Лакомство GreenQzin Дрессура №1 Утка 50г",
+        "Игрушка MPets Мяч-пищалка с хвостом голубой для собак", "Симпарика таблетки для собак З шт"
+    )
+    val printShoppingList= shoppingList.joinToString (separator = ", ")
+    val errorAuthorization = "Ошибка авторизации"
+
+    if (token != null){
+        return printShoppingList
+    } else
+        return errorAuthorization
 }
 
 fun main() {
@@ -26,5 +42,8 @@ fun main() {
     print("Введите пароль: ")
     val password = readln()
 
-    println(authorization(login, password))
+    val token = authorization(log = String(), pass = String())
+    val basket = basket(token = String())
+
+    println(basket)
 }
