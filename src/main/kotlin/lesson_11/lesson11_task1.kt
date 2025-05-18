2723e0bf57a8d4a44543721c5e7d57b9839fa57e
Anzhelika <anzhelika.kuihina@mail.ru>
2025-05-18 22:19:04 +0700

    KS-11-1 переместила в один файл

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,31 +1,38 @@
 package org.example.lesson_11
 
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+)
+
 fun main() {
 
-    val id1: User = User(
+    val user1: User = User(
         id = 125979318,
         login = "Phantom assassin",
         password = "Stifling_Dagger1200",
-        email = "Phantom.assassin@email.ru"
+        email = "Phantom.assassin@mail.ru",
     )
 
-    val id2: User = User(
+    val user2: User = User(
         id = 753020430,
         login = "Windranger",
         password = "Shackleshot1650",
-        email = "Windranger@email.ru"
+        email = "Windranger@mail.ru",
     )
 
-    println(id1.id)
-    println(id1.login)
-    println(id1.password)
-    println(id1.email)
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.email)
 
     println()
 
-    println(id2.id)
-    println(id2.login)
-    println(id2.password)
-    println(id2.email)
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.email)
 
 }
