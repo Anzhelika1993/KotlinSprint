2df94fa200edd4bb182707706232c2dd3ed16ac8
Anzhelika <anzhelika.kuihina@mail.ru>
2025-05-25 13:47:56 +0700

    KS-11-5 форум

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,34 +1,59 @@
 package org.example.lesson_11
 
-import org.example.lesson_10.random
-
-class Forum (
+data class Forum(
     val listOfUsers: MutableList<Member>,
-    val forumMessage: Message,
-    val user: Member,
-){
-    fun createNewUser(name: String = readln(), ) {
+    val messages: MutableList<Message>,
+) {
+    fun createNewUser(name: String = readln()): String {
         val elements = 0..9
         val number = 8
+        var userId = ""
         for (i in 0..number) {
-            elements.random()
-            user.userId.add(random())
+            userId += elements.random()
         }
-        Member(userId = user.userId, userName = name)
-        listOfUsers.add(user)
+        val newUser = Member(userId = userId, userName = name)
+        listOfUsers.add(newUser)
+        return userId
+    }
+
+    fun createNewMessage(memberId: String, text: String) {
+        for (member in listOfUsers) {
+            if (member.userId == memberId) {
+                val newMessege = Message(memberId, text)
+                messages.add(newMessege)
+            }
+        }
+    }
+
+    fun printThread() {
+        for (msg in messages) {
+            for (member in listOfUsers) {
+                if (msg.authorId == member.userId) {
+                    println("${member.userName}: ${msg.message}")
+                }
+            }
+        }
     }
 }
 
-class Member (
-    var userId: MutableList<Int>,
-    val userName: String,
-)
-class Message (
-    val authorId: Member,
-    val message: String,
-)
+data class Member(var userId: String, var userName: String)
+
+data class Message(val authorId: String, val message: String,)
 
 fun main() {
 
+    val forum = Forum(
+        listOfUsers = mutableListOf(),
+        messages = mutableListOf(),
+    )
+    val createdUserId = forum.createNewUser("Overlord")
+    val createdUserId2 = forum.createNewUser("Smurf")
 
+    forum.createNewMessage(createdUserId, "Привет всем!")
+    forum.createNewMessage(createdUserId, "Есть кто?")
+    forum.createNewMessage(createdUserId2, "Привет привет")
+    forum.createNewMessage(createdUserId, "Вы уже слушали новый альбом Антона Маскелиаде?")
+    forum.createNewMessage(createdUserId2, "Нет, кинь ссылку")
+
+    forum.printThread()
 }
