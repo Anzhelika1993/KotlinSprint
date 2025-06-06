c4e8eb2100199ba096612bd85391550b975f4b64
Anzhelika <anzhelika.kuihina@mail.ru>
2025-06-06 20:55:41 +0700

    KS-12-4 убрала геттеры

--- src/main/kotlin/lesson_12/lesson12_task4.kt
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -2,23 +2,22 @@ package org.example.lesson_12
 
 const val ZERO_KELVIN = 273.15f
 
-class WeatherCelsius(_dayTemperature: Int, _nightTemperature: Int, _hasPrecipitations: Boolean) {
-    val dayTemperature = _dayTemperature
-        get() = (field - ZERO_KELVIN).toInt()
-    val nightTemperature = _nightTemperature
-        get() = (field - ZERO_KELVIN).toInt()
-    val hasPrecipitations = _hasPrecipitations
+class WeatherCelsius(dayTemperature: Int, nightTemperature: Int, hasPrecipitations: Boolean) {
+
+    val dayTemperature = dayTemperature - ZERO_KELVIN.toInt()
+    val nightTemperature = nightTemperature - ZERO_KELVIN.toInt()
+    val hasPrecipitations = hasPrecipitations
 
     init {
         println(
-            "Дневная температура: $dayTemperature°C, \n" +
-                    "Ночная температура: $nightTemperature°C, \n" +
-                    "Осадки: $hasPrecipitations"
+            "Дневная температура: ${this@WeatherCelsius.dayTemperature}°C, \n" +
+                    "Ночная температура: ${this@WeatherCelsius.nightTemperature}°C, \n" +
+                    "Осадки: ${this@WeatherCelsius.hasPrecipitations}"
         )
     }
 }
 
 fun main() {
 
-    val friday = WeatherCelsius(_dayTemperature = 303, _nightTemperature = 294, _hasPrecipitations = false)
+    val friday = WeatherCelsius(dayTemperature = 303, nightTemperature = 294, hasPrecipitations = false)
 }
