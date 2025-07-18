bc7fae292f96a79c019584c6fcea3a815488f9b6
Anzhelika <anzhelika.kuihina@mail.ru>
2025-07-18 23:10:43 +0700

    KS-16-5 имитация боя

---
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -0,0 +1,56 @@
+package org.example.lesson_16
+
+fun main() {
+
+    val player = Player("Assassin", 580, 20)
+    player.takeDamage(175)
+    player.takeDamage(269)
+    player.takeHeal(275)
+    player.takeDamage(324)
+    player.takeHeal(140)
+    player.takeDamage(85)
+    player.takeDamage(150)
+    player.takeHeal(275)
+}
+
+class Player(val name: String, private var health: Int, var power: Int) {
+
+    private var currentHealth = health
+    val isAlive: Boolean
+        get() = currentHealth > 0
+
+    fun takeDamage(damage: Int) {
+        if (!isAlive) {
+            onDeath()
+        } else {
+            currentHealth = (currentHealth - damage).coerceAtLeast(0)
+            print("$name получил урон $damage. ")
+            getCurrentHealth()
+        }
+    }
+
+    fun takeHeal(heal: Int) {
+        if (!isAlive) {
+            println("Лечение невозможно. $name убит")
+        } else {
+            currentHealth = (currentHealth + heal).coerceAtMost(health)
+            print("$name получил лечение $heal. ")
+            getCurrentHealth()
+        }
+    }
+
+    private fun onDeath() {
+        health = 0
+        power = 0
+        println("$name убит")
+
+    }
+
+    fun getCurrentHealth() {
+        if (!isAlive) {
+            onDeath()
+            return
+        }
+        println("Текущее здоровье: $currentHealth")
+    }
+}
