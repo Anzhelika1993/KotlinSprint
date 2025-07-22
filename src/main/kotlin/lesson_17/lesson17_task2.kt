3d3384136a0f91f857fd2891e12ad7a1d867ae84
Anzhelika <anzhelika.kuihina@mail.ru>
2025-07-22 23:24:19 +0700

    KS-17-2 сеттер для защиты от изменения имени корабля

---
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_17
+
+fun main() {
+
+    val ship = Ship("Сокол", 75, "Пусан")
+    println("Имя корабля: ${ship.name}")
+    println("Средняя скорость корабля: ${ship.averageSpeed}")
+    println("Порт приписки: ${ship.homePort}")
+
+    ship.name = "Камадо"
+    ship.homePort = "Инчхон"
+
+    println("---после изменений---")
+    println("Имя корабля: ${ship.name}")
+    println("Средняя скорость корабля: ${ship.averageSpeed}")
+    println("Порт приписки: ${ship.homePort}")
+}
+
+class Ship(
+    private val _name: String,
+    val averageSpeed: Int,
+    var homePort: String
+) {
+    var name: String = _name
+        set(value) {
+            println("Нельзя изменить имя корабля")
+        }
+}
