92a719b8cb359f3e386b4a4cee45ae22c36aead5
Anzhelika <anzhelika.kuihina@mail.ru>
2025-07-27 13:55:31 +0700

    KS-17-3 отдельный get для количества

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -5,19 +5,17 @@ fun main() {
     val folder1 = Folder("Приказы", 27, false)
     val folder2 = Folder("Реестры выплат", 49, true)
 
-    println(folder1.name)
-    println(folder2.name)
+    println("Имя папки: ${folder1.name}")
+    println("Имя папки: ${folder2.name}")
+    println("Количество файлов: ${folder1.count}")
+    println("Количество файлов: ${folder2.count}")
 }
 
-class Folder(private val _name: String, private var count: Int, private val isSecret: Boolean) {
+class Folder(private val _name: String, private var _count: Int, private val isSecret: Boolean) {
 
     val name: String
-        get() {
-            return if (isSecret) {
-                count = 0
-                "Скрытая папка. Количество файлов: $count"
-            } else {
-                "Папка: $_name. Количество файлов: $count"
-            }
-        }
+        get() = if (isSecret) "Скрытая папка" else _name
+
+    val count: Int
+        get() = if (isSecret) 0  else _count
 }
